c971992a1f7b36d9296a071b2c9234e169c6d495
Reda Benjelloun <136175741+RmBenjy@users.noreply.github.com>
2024-06-07 19:11:49 +0100

    Update featured.js

--- src/components/sections/featured.js
+++ src/components/sections/featured.js
@@ -1,264 +1,100 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
+import Helmet from 'react-helmet';
 import PropTypes from 'prop-types';
-import Img from 'gatsby-image';
-import sr from '@utils/sr';
-import { srConfig } from '@config';
-import { FormattedIcon } from '@components/icons';
+import anime from 'animejs';
+import { IconLoader } from '@components/icons';
 import styled from 'styled-components';
-import { theme, mixins, media, Section, Heading } from '@styles';
-import { Link } from 'gatsby';
+import { theme, mixins } from '@styles';
+const { colors } = theme;
 
-const { colors, fontSizes, fonts } = theme;
-
-const StyledContainer = styled(Section)`
+const StyledContainer = styled.div`
   ${mixins.flexCenter};
-  flex-direction: column;
-  align-items: flex-start;
-`;
-
-const StyledProject = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 100px;
-
-  ${media.thone`
-    margin-bottom: 70px;
-  `};
-`;
-
-const StyledContent = styled.div`
-  position: relative;
-  grid-column: 1 / 7;
-  grid-row: 1 / -1;
-
-  ${media.thone`
-    grid-column: 1 / -1;
-    padding: 40px 40px 30px;
-    z-index: 5;
-  `};
-
-  ${media.phablet`
-    padding: 30px 25px 20px;
-  `};
-`;
-
-const StyledLabel = styled.h4`
-  font-size: ${fontSizes.smish};
-  font-weight: normal;
-  color: ${colors.green};
-  font-family: ${fonts.SFMono};
-  margin-top: 10px;
-  padding-top: 0;
-`;
-
-const StyledProjectName = styled.h5`
-  font-size: 28px;
-  margin: 0 0 20px;
-  color: ${colors.lightestSlate};
-
-  ${media.tablet`
-    font-size: 24px;
-  `};
-
-  ${media.thone`
-    color: ${colors.white};
-  `};
-
-  a {
-    ${media.tablet`
-      display: block;
-    `};
-  }
-`;
-
-const StyledDescription = styled.div`
-  ${mixins.boxShadow};
-  position: relative;
-  z-index: 2;
-  padding: 25px;
-  background-color: ${colors.lightNavy};
-  color: ${colors.lightSlate};
-  font-size: ${fontSizes.lg};
-  border-radius: ${theme.borderRadius};
-
-  ${media.thone`
-    background-color: transparent;
-    padding: 20px 0;
-    box-shadow: none;
-
-    &:hover {
-      box-shadow: none;
-    }
-  `};
-
-  p {
-    margin: 0;
-  }
-
-  a {
-    ${mixins.inlineLink};
-  }
-`;
-
-const StyledTechList = styled.ul`
-  position: relative;
-  z-index: 2;
-  display: flex;
-  flex-wrap: wrap;
-  padding: 0;
-  margin: 25px 0 10px;
-  list-style: none;
-
-  li {
-    font-family: ${fonts.SFMono};
-    font-size: ${fontSizes.smish};
-    color: ${colors.green};
-    margin-right: ${theme.margin};
-    margin-bottom: 7px;
-    white-space: nowrap;
-
-    &:last-of-type {
-      margin-right: 0;
-    }
-
-    ${media.thone`
-      color: ${colors.green};
-      margin-right: 10px;
-    `};
-  }
-`;
-
-const StyledLinkWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  position: relative;
-  margin-top: 10px;
-  margin-left: -10px;
-  color: ${colors.lightestSlate};
-
-  a {
-    padding: 10px;
-
-    svg {
-      width: 22px;
-      height: 22px;
-    }
-  }
-`;
-
-const StyledFeaturedImg = styled(Img)`
+  background-color: ${colors.darkNavy};
+  position: fixed;
   width: 100%;
-  max-width: 80%;
-  vertical-align: middle;
-  border-radius: ${theme.borderRadius};
-  position: relative;
-  mix-blend-mode: multiply;
-  margin-left: auto;
-  margin-right: auto;
-
-  ${media.tablet`
-    object-fit: cover;
-    width: auto;
-    height: 100%;
-    
-  `};
-
-  ${media.thone`
-    max-width: 100%; /* Set the maximum width for small screens */
-  `};
+  height: 100%;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 99;
 `;
-
-const StyledImgContainer = styled.a`
-  grid-column: 6 / -1;
-  grid-row: 1 / -1;
-  position: relative;
-  z-index: 1;
-  border-radius: ${theme.radius + 1}px;
+const StyledLogo = styled.div`
+  width: max-content;
+  max-width: 100px;
   transition: ${theme.transition};
-
-  ${media.tablet`
+  opacity: ${props => (props.isMounted ? 1 : 0)};
+  svg {
+    width: 100%;
     height: 100%;
-  `};
-
-  ${media.thone`
-    grid-column: 1 / -1;
-    margin-top: 20px;
-  `};
+    display: block;
+    margin: 0 auto;
+    fill: none;
+    user-select: none;
+    #B {
+      opacity: 0;
+    }
+  }
 `;
 
-const Featured = ({ data }) => {
-  const featuredProjects = data.filter(({ node }) => node);
+const Loader = ({ finishLoading }) => {
+  const animate = () => {
+    const loader = anime.timeline({
+      complete: () => finishLoading(),
+    });
 
-  const revealTitle = useRef(null);
-  const revealProjects = useRef([]);
+    loader
+      .add({
+        targets: '#logo path',
+        delay: 100, // Reduced from 300 to 100
+        duration: 1000, // Reduced from 1500 to 1000
+        easing: 'easeInOutQuart',
+        strokeDashoffset: [anime.setDashoffset, 0],
+      })
+      .add({
+        targets: '#logo #B',
+        duration: 500, // Reduced from 700 to 500
+        easing: 'easeInOutQuart',
+        opacity: 1,
+      })
+      .add({
+        targets: '#logo',
+        delay: 300, // Reduced from 500 to 300
+        duration: 200, // Reduced from 300 to 200
+        easing: 'easeInOutQuart',
+        opacity: 0,
+        scale: 0.1,
+      })
+      .add({
+        targets: '.loader',
+        duration: 100, // Reduced from 200 to 100
+        easing: 'easeInOutQuart',
+        opacity: 0,
+        zIndex: -1,
+      });
+  };
+
+  const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
-    sr.reveal(revealTitle.current, srConfig());
-    revealProjects.current.forEach((ref, i) => sr.reveal(ref, srConfig(i * 100)));
+    const timeout = setTimeout(() => setIsMounted(true), 10);
+    animate();
+    return () => clearTimeout(timeout);
   }, []);
 
   return (
-    <StyledContainer id="projects">
-      <Heading ref={revealTitle}>Some Things I&apos;ve Worked On</Heading>
+    <StyledContainer className="loader">
+      <Helmet bodyAttributes={{ class: `hidden` }} />
 
-      <div>
-        {featuredProjects &&
-          featuredProjects.map(({ node }, i) => {
-            const { frontmatter, html } = node;
-            const { external, title, tech, github, cover } = frontmatter;
-
-            return (
-              <StyledProject key={i} ref={el => (revealProjects.current[i] = el)}>
-                <StyledContent>
-                  <StyledLabel>Featured Project</StyledLabel>
-                  <StyledProjectName>
-                    {external ? (
-                      <a
-                        href={external}
-                        target="_blank"
-                        rel="nofollow noopener noreferrer"
-                        aria-label="External Link">
-                        {title}
-                      </a>
-                    ) : (
-                      title
-                    )}
-                  </StyledProjectName>
-                  <StyledDescription dangerouslySetInnerHTML={{ __html: html }} />
-
-                  {tech && (
-                    <StyledTechList>
-                      {tech.map((tech, i) => (
-                        <li key={i}>{tech}</li>
-                      ))}
-                    </StyledTechList>
-                  )}
-
-                  <StyledLinkWrapper>
-                    <Link to="/404">
-                      {' '}
-                      {/* Change "/404" to the actual path of your 404 page */}
-                      <FormattedIcon name="External" />
-                    </Link>
-                  </StyledLinkWrapper>
-                </StyledContent>
-
-                <StyledImgContainer
-                  href={external ? external : github ? github : '#'}
-                  target="_blank"
-                  rel="nofollow noopener noreferrer">
-                  <StyledFeaturedImg fluid={cover.childImageSharp.fluid} alt={title} />
-                </StyledImgContainer>
-              </StyledProject>
-            );
-          })}
-      </div>
+      <StyledLogo isMounted={isMounted}>
+        <IconLoader />
+      </StyledLogo>
     </StyledContainer>
   );
 };
 
-Featured.propTypes = {
-  data: PropTypes.array.isRequired,
+Loader.propTypes = {
+  finishLoading: PropTypes.func.isRequired,
 };
 
-export default Featured;
+export default Loader;
