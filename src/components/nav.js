b625d06c27d4e623ec0331f81bc300e4c769d2cb
Reda Benjelloun <136175741+RmBenjy@users.noreply.github.com>
2024-06-07 18:17:32 +0100

    Update nav.js

--- src/components/nav.js
+++ src/components/nav.js
@@ -294,7 +294,7 @@ class Nav extends Component {
                 <CSSTransition classNames={fadeDownClass} timeout={timeout}>
                   <div style={{ transitionDelay: `${isHome ? navLinks.length * 100 : 0}ms` }}>
                     <StyledResumeButton
-                      href="/resume.pdf"
+                      href="https://drive.google.com/file/d/1SGx37eRHamps_pQz-iL1MyKfKa5JghWO/view?usp=drive_link"
                       target="_blank"
                       rel="nofollow noopener noreferrer">
                       Resume
